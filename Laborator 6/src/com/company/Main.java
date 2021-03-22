2047d102c686ff3cb5de7af9dd5d6d5904c0ec72
andreeabmanea <75753307+andreeabmanea@users.noreply.github.com>
2021-03-22 09:50:47 +0200

    Add files via upload

---
+++ Laborator 6/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        MyFrame f = new MyFrame("Main Frame");
+
+
+    }
+}
---
+++ Laborator 6/src/com/company/MyCanvas.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.awt.*;
+
+public class MyCanvas extends Canvas {
+    public MyCanvas(){
+        setBackground(Color.BLACK);
+        setSize(200,200);
+    }
+}
