b50eab736ee29cd40ab20e62d77fecf5793be875
andreeabmanea <75753307+andreeabmanea@users.noreply.github.com>
2021-04-12 09:43:13 +0300

    Add files via upload

--- Laborator 8/film.sql
+++ Laborator 8/film.sql
@@ -1,12 +1,11 @@
-DROP TABLE movies CASCADE CONSTRAINTS;
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS genres;
 
-DROP TABLE genres CASCADE CONSTRAINTS;
-
-DROP TABLE guide CASCADE CONSTRAINTS;
+DROP TABLE IF EXISTS guide;
 
 CREATE TABLE movies (
 	id INT PRIMARY KEY,
-        title VARCHAR2(64) NOT NULL, 
+        title VARCHAR(64) NOT NULL, 
         release_date DATE,
 	duration INT,
 	score DOUBLE(5,4)
@@ -14,13 +13,13 @@ CREATE TABLE movies (
 
 CREATE TABLE genres (
 	id INT PRIMARY KEY,
-        name VARCHAR2(25)
+        name VARCHAR(25)
 );
 
 CREATE TABLE guide (
 	id_movie INT NOT NULL,
 	id_genre INT NOT NULL,
 	CONSTRAINT fk_id_movie FOREIGN KEY(id_movie) REFERENCES movies(id),
-	CONSTRAINT fk_id_genre FOREIGN KEY(id_genre) REFERENCES genres(id)
+	CONSTRAINT fk_id_genre FOREIGN KEY(id_genre) REFERENCES genres(id),
 	CONSTRAINT uni UNIQUE(id_movie, id_genre) 
 );
